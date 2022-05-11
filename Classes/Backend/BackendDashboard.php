0323cb4bbc6369c1af6b72d3108c417314123672
sebastopolys <sebastopolys@gmail.com>
2022-05-11 18:37:41 -0300

    new class BackendRender

--- Classes/Backend/BackendDashboard.php
+++ Classes/Backend/BackendDashboard.php
@@ -12,37 +12,25 @@ namespace App\Backend;
 * called from: ApiUsers.php
 **/
 
-class BackendDashboard
+class BackendDashboard extends BackendRender
 {
-    public $option = null;
+    
     /*
     * Initialize with hooks
     */
     public function backHooks()
     {
         $menu = new BackendMenu();
-        $menu->runMenuHook();
-        
+        $menu->runMenuHook();        
         add_action('admin_init', [$this, 'apiusersSettingsInit']);
-    }   
-
-    /*
-    *  Get backend options
-    */
-    public function backOps(){
-        if($this->option == null):
-            $obj = new BackendOptions();
-            $this->option = $obj->option;
-        endif;
-        return $this->option;
-    }
+    }       
    
     /*
     * Add WP API settings on Settings page
     */
     public function apiusersSettingsInit()
     {     
-        register_setting('pluginPage', 'apiusers_settings', [$this, 'callbackValidation']);
+        register_setting('pluginPage', 'apiusers_settings',[$this,'callbackValidation']);
 
         add_settings_section(
             'apiusers_pluginPage_section',
@@ -73,87 +61,5 @@ class BackendDashboard
             'pluginPage',
             'apiusers_pluginPage_section'
         );
-    }
-   
-    /*
-    * Adds sub title to settings Section
-    */
-    public function apiusersSettingsSectionCallback()
-    {
-        echo wp_kses_post('<p class="warn-message"><i>
-        *Some options might not work properly with untested themes</i></p>');
-    }
-
-    /*
-    * Custom endpoint field
-    */
-    public function apiusersTextFieldRender()
-    {   
-        ?>
-        <input type='text' name='apiusers_settings[apiusers_text_field_0]' 
-        value='<?php echo esc_attr($this->backOps()['apiusers_text_field_0']); ?>'>
-        <?php
-    }
-
-    /*
-    * View field
-    */
-    public function apiUsersRadioRender()
-    {         
-        ?>
-        <input id='radiotheme' type='radio' name='apiusers_settings[apiusers_radio_field_1]'
-        <?php
-        if ( $this->backOps()['apiusers_radio_field_1'] === 'theme') {
-            ?> checked<?php
-        }
-        ?>
-        value='theme'>
-        <label for="radiotheme">Theme</label>
-        <input id='radioraw' type='radio' name='apiusers_settings[apiusers_radio_field_1]'
-        <?php if ( $this->backOps()['apiusers_radio_field_1'] === 'raw') {
-            ?> checked<?php
-        }
-        ?>
-        value='raw'>
-        <label for='radioraw'>Raw</label>
-        <?php
-    }
-
-    /*
-    * Credits field
-    */
-    public function apiusersCheckboxRender()
-    {
-        if (empty($this->backOps())) {
-            $this->backOps()['apiusers_checkbox_field_2'] = '';
-        }
-        ?>
-        <input type='checkbox' name='apiusers_settings[apiusers_checkbox_field_2]' <?php
-        if (isset($this->backOps()['apiusers_checkbox_field_2'])) {
-            ?>checked  <?php
-        } ?>value = "1" 
-        >
-        <?php
-    }
-
-    /*
-    * Custom Endpoint string REGEX validation
-    */
-    public function callbackvalidation(array $input): array
-    {
-        $output = [];
-        if (isset($input['apiusers_text_field_0'])) {
-            $output['apiusers_text_field_0'] =
-            preg_replace('/\s+/', '_', $input['apiusers_text_field_0']);
-            $output['apiusers_text_field_0'] =
-            preg_replace('/[^_A-Za-z0-9]/ ', '', $output['apiusers_text_field_0']);
-            if ($output['apiusers_text_field_0'] === '') {
-                $output['apiusers_text_field_0'] =
-                get_option('apiusers_settings')['apiusers_text_field_0'];
-            }
-            $output['apiusers_radio_field_1'] = $input['apiusers_radio_field_1'];
-            $output['apiusers_checkbox_field_2'] = $input['apiusers_checkbox_field_2'];
-            return apply_filters('callbackvalidation', $output, $input);
-        }
     }
 }
---
+++ Classes/Backend/BackendRender.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Backend;
+
+/**
+* Class BackendMenu
+*
+* @package App\Backend
+*
+* called from: BackendDashboard.php
+**/
+
+class BackendRender{
+
+    public $option = null;
+
+    /*
+    *  Get backend options
+    */
+    public function backOps(){
+        if($this->option == null):
+            $obj = new BackendOptions();
+            $this->option = $obj->option;
+        endif;
+        return $this->option;
+    }
+
+     /*
+    * Adds sub title to settings Section
+    */
+    public function apiusersSettingsSectionCallback()
+    {
+        echo wp_kses_post('<p class="warn-message"><i>
+        *Some options might not work properly with untested themes</i></p>');
+    }
+
+    /*
+    * Custom endpoint field
+    */
+    public function apiusersTextFieldRender()
+    {   
+        ?>
+        <input type='text' name='apiusers_settings[apiusers_text_field_0]' 
+        value='<?php echo esc_attr($this->backOps()['apiusers_text_field_0']); ?>'>
+        <?php
+    }
+
+    /*
+    * View field
+    */
+    public function apiUsersRadioRender()
+    {         
+        ?>
+        <input id='radiotheme' type='radio' name='apiusers_settings[apiusers_radio_field_1]'
+        <?php
+        if ( $this->backOps()['apiusers_radio_field_1'] === 'theme') {
+            ?> checked<?php
+        }
+        ?>
+        value='theme'>
+        <label for="radiotheme">Theme</label>
+        <input id='radioraw' type='radio' name='apiusers_settings[apiusers_radio_field_1]'
+        <?php if ( $this->backOps()['apiusers_radio_field_1'] === 'raw') {
+            ?> checked<?php
+        }
+        ?>
+        value='raw'>
+        <label for='radioraw'>Raw</label>
+        <?php
+    }
+
+    /*
+    * Credits field
+    */
+    public function apiusersCheckboxRender()
+    {
+        if (empty($this->backOps())) {
+            $this->backOps()['apiusers_checkbox_field_2'] = '';
+        }
+        ?>
+        <input type='checkbox' name='apiusers_settings[apiusers_checkbox_field_2]' <?php
+        if (isset($this->backOps()['apiusers_checkbox_field_2'])) {
+            ?>checked  <?php
+        } ?>value = "1" 
+        >
+        <?php
+    }
+
+     /*
+    * Custom Endpoint string REGEX validation
+    */
+    public function callbackvalidation(array $input): array
+    {
+        $output = [];
+        if (isset($input['apiusers_text_field_0'])) {
+            $output['apiusers_text_field_0'] =
+            preg_replace('/\s+/', '_', $input['apiusers_text_field_0']);
+            $output['apiusers_text_field_0'] =
+            preg_replace('/[^_A-Za-z0-9]/ ', '', $output['apiusers_text_field_0']);
+            if ($output['apiusers_text_field_0'] === '') {
+                $output['apiusers_text_field_0'] =
+                get_option('apiusers_settings')['apiusers_text_field_0'];
+            }
+            $output['apiusers_radio_field_1'] = $input['apiusers_radio_field_1'];
+            $output['apiusers_checkbox_field_2'] = $input['apiusers_checkbox_field_2'];
+            return apply_filters('callbackvalidation', $output, $input);
+        }
+    }
+}
